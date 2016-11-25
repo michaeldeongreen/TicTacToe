92f1ead0b85647f93c53b42105979a7abd9891ed
michaeldeongreen <michael.green@architectinginnovation.com>
2016-11-25 06:10:38 -0600

    *Added new enum GameResult

---
+++ src/Tic.Tac.Toe.App.Services/Enumerations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic.Tac.Toe.App.Services
+{
+    public static class Enumerations
+    {
+        public enum ResultType
+        {
+            Winner = 1,
+            NoWinner = 2,
+            Deadlock = 3
+        }
+    }
+}
